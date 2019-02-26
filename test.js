93be5204cfd789f0b59d50add22ad5d1255c8b33
Benjamin Poncet <benjamin.poncet@outlook.fr>
2019-02-26 23:19:03 +0100

    Add all supported language

--- test.js
+++ test.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 const toml = require('toml');
 const snipsConfig = toml.parse(fs.readFileSync('/etc/snips.toml', 'utf-8'));
 
+var textToSpeech = [];
+textToSpeech[0] = { lang: "en", text: "Hello Bobby" };
+textToSpeech[1] = { lang: "fr", text: "Bonjour Bobby" };
+textToSpeech[2] = { lang: "it", text: "Ciao Bobby" };
+textToSpeech[3] = { lang: "es", text: "Hola Bobby" };
+textToSpeech[4] = { lang: "de", text: "Hallo Bobby" };
+textToSpeech[5] = { lang: "ja", text: "こんにちはボビー" };
+textToSpeech[6] = { lang: "ko", text: "안녕, 바비" };
+var i=0;
+
 var mqttHostname = "mqtt://localhost";
 var mqttOptions = {};
 
@@ -14,8 +24,10 @@ var clientMQTT  = mqtt.connect(mqttHostname, mqttOptions);
 
 clientMQTT.on('connect', function () {
 	console.log("[Bobby Snips TTS Test Log] Connected to MQTT broker " + mqttHostname);
-  setInterval(function(){
-    console.log("[Bobby Snips TTS Test Log] Send Text to TTS");
-    clientMQTT.publish("hermes/tts/say", '{"siteId": "default", "sessionId": "123", "text": "Bonjour gros", "lang": "fr-FR", "id":"hermes/tts/sayFinished"}');
-  },5000);
+	setInterval(function(){
+		if(i == textToSpeech.length ) i = 0;
+		console.log("[Bobby Snips TTS Test Log] Send Text to TTS "+textToSpeech[i].lang);
+  	clientMQTT.publish("hermes/tts/say", '{"siteId": "default", "sessionId": "test-'+textToSpeech[i].lang+'", "text": "'+textToSpeech[i].text+'", "lang": "'+textToSpeech[i].lang+'", "id":"'+i+'"}');
+		i++;
+	}, 5000);
 });
